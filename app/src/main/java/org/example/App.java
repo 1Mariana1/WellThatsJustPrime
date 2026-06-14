8e705d7c67aae5fae52379ffd21c97ceaf0ac6ed
Mariana <gm83746@mail.harpercollege.edu>
2026-06-14 14:01:49 -0500

    WellThatsJustPrime

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,5 +3,14 @@ package org.example;
 public class App {
   public static void main(String[] args) {
     // Test your Factorizer class using a driver program here!
+    Factorizer factorizer = new Factorizer();
+
+    System.out.println(factorizer.primeFactors(0));
+    System.out.println(factorizer.primeFactors(1));
+    System.out.println(factorizer.primeFactors(9));
+    System.out.println(factorizer.primeFactors(10));
+    System.out.println(factorizer.primeFactors(12));
+    System.out.println(factorizer.primeFactors(30));
+
   }
 }
--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -1,13 +1,46 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FactorizerTest {
+
   @Test
   void itReturnsAnEmptyListForZero() {
     Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+    assertEquals(new ArrayList<Integer>(), factorizer.primeFactors(0));
   }
+
+  @Test
+  void itReturnsAnEmptyListForOne() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(new ArrayList<Integer>(), factorizer.primeFactors(1));
+  }
+
+  @Test
+  void itReturnsPrimeFactorsForNine() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(new ArrayList<>(Arrays.asList(3, 3)), factorizer.primeFactors(9));
+  }
+
+  @Test
+  void itReturnsPrimeFactorsForTen() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(new ArrayList<>(Arrays.asList(2, 5)), factorizer.primeFactors(10));
+  }
+
+  @Test
+  void itReturnsPrimeFactorsForTwelve() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(new ArrayList<>(Arrays.asList(2, 2, 3)), factorizer.primeFactors(12));
+  }
+
+  @Test
+  void itReturnsPrimeFactorsForThirty() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(new ArrayList<>(Arrays.asList(2, 3, 5)), factorizer.primeFactors(30));
+  }
+
 }
